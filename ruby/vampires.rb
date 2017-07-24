70e4ee6e3797afdfa7d5b20a49ba567d4410bf5e
Marlo Ito <marloakemi@gmail.com>
2017-07-23 17:39:25 -0700

    adding intiital questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,11 @@
+puts "What's your name?"
+applicant_name = gets.chomp
+puts "How old are you?"
+applicant_age = gets.chomp
+puts "What year were you born?"
+applicant_year_born = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+applicant_want_garlic = gets.chomp
+puts "Would you like to enroll in the company’s health insurance?"
+applicant_want_insurance = gets.chomp
+

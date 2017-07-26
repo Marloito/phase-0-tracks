152d8faee8d65c09b5980a0487c050f167b76c02
Marlo Ito <marloakemi@gmail.com>
2017-07-25 21:40:20 -0700

    finish assignment all content

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -37,3 +37,107 @@ numbers.each do |spell, digit|
 end
 p numbers
 
+#Release 2-------------------------------------------------------------
+
+#1---------
+a = ["a", "b", "c"]
+
+h = {
+	a: 100,
+	b: 200,
+	c: 300
+}
+
+p a 
+puts "DELETE IF = B"
+a.delete_if { |letter| letter == "b" }
+puts "new array is"
+p a
+
+puts ""
+
+p h
+puts "DELETE IF >= 201"
+h.delete_if { |key, value| value >= 201 }
+puts "new hash is"
+p h
+
+#2----------
+a = ["a", "b", "c"]
+
+h = {
+	a: 100,
+	b: 200,
+	c: 300
+}
+
+puts ""
+
+p a
+puts "KEEP IF >= B"
+a.keep_if { |letter| letter >= "b" }
+puts "new array is"
+p a
+
+puts ""
+
+p h
+puts "KEEP IF >=201"
+h.keep_if { |key, value| value >= 201 }
+puts "new hash is"
+p h
+
+puts ""
+#3----------------------
+a = ["a", "b", "c"]
+
+h = {
+	a: 100,
+	b: 200,
+	c: 300
+}
+
+p a
+a.reject! { |letter| letter >= "b" }
+p a
+
+puts ''
+
+p h
+h.reject! { |key, value| value >= 201 }
+p h
+
+puts ""
+
+#4-------------------------
+a = ["a", "b", "c"]
+
+h = {
+	a: 100,
+	b: 200,
+	c: 300
+}
+
+p a 
+p "we put in this DROP"
+p a.drop_while { |letter| letter < "b" }
+a.drop_while { |letter| letter < "b" }
+p a 
+
+=begin
+
+puts ""
+
+p a 
+p "we put in this TAKE"
+p a.take_while { |letter| letter < "b" }
+a.take_while { |letter| letter < "b" }
+p a 
+
+=end
+puts ""
+
+p h 
+p h.to_a.drop_while { |arr| arr[1] < 199 }.to_h
+h.to_a.drop_while { |arr| arr[1] < 201 }.to_h
+p h

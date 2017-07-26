c01b8a0149df7f963af5288a9a6697d701965590
Marlo Ito <marloakemi@gmail.com>
2017-07-25 18:10:50 -0700

    getting input - updating hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -7,3 +7,30 @@ client_info = {
 	has_kids: ""
 }
 
+#intro
+puts "Hello, in order to get started deigning your space, we will first need to gather some information from you."
+puts	""
+#get name - input into hash
+puts "What is your full name?"
+client_info[:name] = gets.chomp
+#get age - input into hash
+puts "How old are you?"
+client_info[:age] = gets.chomp.to_i
+#get favorite color - input into hash
+puts "What is your favorite color?"
+client_info[:color] = gets.chomp
+#get theme - input into hash
+puts "What theme would you like to follow?"
+client_info[:theme] = gets.chomp
+#get has_kids - input into hash 
+puts "Do you have kids? (y/n)"
+kids_input = gets.chomp 
+if	kids_input == "y"
+	kids_input = true
+else
+	kids_input = false
+end		
+client_info[:has_kids] = kids_input
+
+#print out 
+p client_info

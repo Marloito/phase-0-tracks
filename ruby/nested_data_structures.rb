df9e3d8e73b11adc790c899c22e0fe79bee2104e
Marlo Ito <marloakemi@gmail.com>
2017-07-30 17:46:48 -0700

    fixing syntax

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,33 +1,33 @@
 friends = {
 	sara: {
-		nickname: "sare bear"
+		nickname: "sare bear",
 		location: {
-			state: "WA"
+			state: "WA",
 			city: "Tacoma"
-		}
+		},
 		favorite_foods: [
 			"apple",
 			"banana",
 			"pear"
 		]
-	}
+	},
 	greg: {
-		nickname: "gregy bear"
+		nickname: "gregy bear",
 		location: {
-			state: "WA"
+			state: "WA",
 			city: "Seattle"
-		}
+		},
 		favorite_foods: [
 			"chicken",
 			"apple"
 		]
-	}
+	},
 	victoria: {
-		nickname: "mikky vikky"
+		nickname: "mikky vikky",
 		location: {
-			state: "WA"
+			state: "WA",
 			city: "Olympia"
-		}
+		},
 		favorite_foods: [
 			"orange",
 			"ice cream"

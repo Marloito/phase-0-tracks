29598b5f3bb9acc75dc6bcb879d17f717b1e88b0
Marlo Ito <marloakemi@gmail.com>
2017-07-30 17:43:37 -0700

    adding nested data

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,36 @@
+friends = {
+	sara: {
+		nickname: "sare bear"
+		location: {
+			state: "WA"
+			city: "Tacoma"
+		}
+		favorite_foods: [
+			"apple",
+			"banana",
+			"pear"
+		]
+	}
+	greg: {
+		nickname: "gregy bear"
+		location: {
+			state: "WA"
+			city: "Seattle"
+		}
+		favorite_foods: [
+			"chicken",
+			"apple"
+		]
+	}
+	victoria: {
+		nickname: "mikky vikky"
+		location: {
+			state: "WA"
+			city: "Olympia"
+		}
+		favorite_foods: [
+			"orange",
+			"ice cream"
+		]
+	}
+}

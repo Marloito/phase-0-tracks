a60ee05ebf576be9c18cd9e26220960f2e417d42
Marlo Ito <marloakemi@gmail.com>
2017-07-31 08:07:53 -0700

    creating program

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,49 @@
+vowels = ["a", "e", "i", "o", "u"]
+consenents = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+
+#a = vowels[0]
+
+
+
+puts "Please type your full name."
+
+#take name as input
+var_name = gets.chomp
+var_name.downcase!
+
+#break name into words (array)
+var_name = var_name.split
+
+#switch first and last words 
+var_name[0], var_name[2] = var_name[2], var_name[0]
+
+#break name into letters
+var_name = var_name.join(' ').split('')
+
+#if vowel change to next vowel
+#if not then go to next consenent
+
+var_name.each do |letter|
+	if vowels.include?(letter)
+		if letter == vowels.last
+			letter = vowels.first
+		else	
+			var_name[var_name.index { |x| x.include?(letter) }] = vowels[vowels.index { |x| x.include?(letter) } + 1] 
+		end	
+	elsif consenents.include?(letter)
+		if letter == consenents.last
+			letter = consenents.first
+		else	
+			var_name[var_name.index { |x| x.include?(letter) }] = consenents[consenents.index { |x| x.include?(letter) } + 1] 
+		end	
+	end			
+end
+
+var_name = var_name.join.split
+
+var_name.map! { |name| name.capitalize }
+
+var_name = var_name.join(' ')
+
+p "New name: #{var_name}"		
+

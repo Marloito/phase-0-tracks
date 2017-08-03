ead2471a1f96e6793c063c730de7bf00ac268c65
Marlo Ito <marloakemi@gmail.com>
2017-08-02 17:35:53 -0700

    initial commit

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+#Species--------------
+#Canine
+
+#Characteristics-------------
+#Eye Color: Varies
+#Color: Varies
+#Nose: Moist
+#Name: Vaires
+#Leg Count: 4
+#Friendly: Yes
+
+#Behavior-----------------------
+#Jump
+#Lick
+#Bark
+#Nap

9b3fce66738e30506032f0d6df6dfa1ae61ee3b8
Marlo Ito <marloakemi@gmail.com>
2017-08-07 02:23:43 -0700

    adding psuedocode

---
+++ ruby/game/game.rb
@@ -0,0 +1,11 @@
+#word input becomes secret word
+#until user makes number of guesses equivalent to letters in word
+	#if input letter is in word 
+	 #letter shows up
+	 #asked to input another letter
+	#if input letter is not in word
+	 #taunt
+	 #askes to input another letter
+	#if guess correct word 
+		#game ends  
+		

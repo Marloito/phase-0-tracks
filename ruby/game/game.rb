5cc92abad203d8986e2074ddbe273fe4c12430e4
Marlo Ito <marloakemi@gmail.com>
2017-08-12 18:37:08 -0700

    why is correct_guess marking as 'true'?

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,6 +1,6 @@
-#word input becomes secret word
+
 #until user makes number of guesses equivalent to letters in word
-	#if input letter is in word 
+	#if input letter is in word #word input becomes secret word
 	 #letter shows up
 	 #asked to input another letter
 	#if input letter is not in word
@@ -8,35 +8,68 @@
 	 #askes to input another letter
 	#if guess correct word 
 		#game ends  
+class WordGame
+  attr_accessor :original_word, :guess
+  attr_reader :guess_count, :correct_guess
 
-original_word = "hello"
-word = original_word.split("")
+  def initialize
+    @original_word = ""
+    @guess_count = 0
+    @correct_guess = false
+    @guess = ""
+  end 
 
-puts "original_word: #{original_word}"
-puts "word: #{word}"
+  def check_entry
+    @guess_count += 1
+    if @original_word == @guess 
+      @correct_guess = true
+    else 
+      false
+    end
+  end
 
-puts
+end    
 
-guess = "l"
+puts "Player 1, enter a word!"
+game = WordGame.new
 
-p original_word == guess
+@original_word = gets.chomp
 
-if original_word == guess 
-  puts "Correct! The secret word was:"
-elsif original_word.include? guess 
-	word.map! do |letter|
-		if letter == guess
-			letter
-		else
-			"-"
-		end
-	end
-else puts "Try again!"
-  word.map! { |letter| "-"}
+word = @original_word.split("")
+
+puts "Player 2, the secret word has #{word.length} letters."
+puts "You have #{word.length} chances, good luck!"
+
+until game.correct_guess || game.guess_count == @original_word.length
+  puts "Guess the secret word, or a letter."
+  @guess = gets.chomp
+  if !game.check_entry && game.guess_count != @original_word.length
+    if @original_word.include? @guess 
+      word.map! do |letter|
+        if letter == @guess
+          letter
+        else
+          "-"
+        end
+      end
+      word = @original_word.split("")
+    else puts "Try again!"
+      word.map! { |letter| "-"}
+      word = @original_word.split("")
+    end
+  end  
 end
 
+if game.correct_guess
+  puts "Correct! You won in #{game.guess_count} guesses! The secret word was "
+else
+  puts "You lose! Better luck next time!"
+end 
+
 word.each { |letter| print letter }
-word = original_word.split("")
+
+puts @guess
+puts @original_word
 
 
 

9ce9ff3f41f594a1342073e5263fc05520df8fbb
Marlo Ito <marloakemi@gmail.com>
2017-08-13 19:11:15 -0700

    adding conversion to object functionality

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,6 +1,7 @@
+//RELEASE 0
 var colors = ["blue", "green", "yellow","red"];
 
-var names = ["Sal", "Mystic", "Flash", "Ed"]
+var names = ["Sal", "Mystic", "Flash", "Ed"];
 
 console.log(colors)
 console.log(names)
@@ -10,3 +11,16 @@ names.push("Fire");
 
 console.log(colors)
 console.log(names)
+
+//RELEASE 1
+//console.log(names[0])  //using as test code
+
+var horses = {}
+
+var length = colors.length
+
+for (var index = 0; index < length; index++ ) {
+	horses[names[index]] = colors[index];
+}
+
+console.log(horses)

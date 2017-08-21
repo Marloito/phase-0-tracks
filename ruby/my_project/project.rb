9137d3a543d9470817d9ff9b85b64d62034cd449
Marlo Ito <marloakemi@gmail.com>
2017-08-21 04:05:41 -0700

    all examples uploaded

--- ruby/my_project/project.rb
+++ ruby/my_project/project.rb
@@ -16,12 +16,21 @@ db.execute(some_command)
 
 # db.execute("INSERT INTO example (name, age) VALUES ('Bob', 5)")
 
-#show the data you are working with
-example = db.execute("SELECT * FROM example")
-p example.class
-p example
 
+example = db.execute("SELECT * FROM example")
 #using ruby to work with data
 example.each do |example|
 	puts "#{example[0]} is #{example[1]}."
 end	
+
+def create_data(db, name, age)
+	db.execute("INSERT INTO example (name, age) VALUES (?, ?)", [name, age])
+end	
+
+#100.times do 
+#	create_data(db, Faker::Name.name, 0)
+#end	
+
+#show the data you are working with
+#p example.class
+#p example
